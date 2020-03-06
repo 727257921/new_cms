0dcd03f9271fbffeb7d1724cf022db485926dd6c
root <727257921@qq.com>
2020-03-06 11:33:09 +0800

    '初始化提交'

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,34 @@
+<?php if (!defined('THINK_PATH')) exit(); $config = D("Basic")->select(); $navs = D("Menu")->getBarMenus(); ?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo ($config["title"]); ?></title>
+  <meta name="keywords" content="<?php echo ($config["keywords"]); ?>" />
+  <meta name="description" content="<?php echo ($config["description"]); ?>" />
+  <link rel="stylesheet" href="/Public/css/bootstrap.min.css" type="text/css" />
+  <link rel="stylesheet" href="/Public/css/home/main.css" type="text/css" />
+</head>
+<body>
+<header id="header">
+  <div class="navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="/">
+          <img src="/Public/images/logo.png" alt="">
+        </a>
+      </div>
+      <ul class="nav navbar-nav navbar-left">
+        <li><a href="/" <?php if($result['catId'] == 0): ?>class="curr"<?php endif; ?>>首页</a></li>
+        <?php if(is_array($navs)): foreach($navs as $key=>$vo): ?><li><a href="/index.php?c=cat&id=<?php echo ($vo["menu_id"]); ?>" <?php if($vo['menu_id'] == $result['catId']): ?>class="curr"<?php endif; ?>><?php echo ($vo["name"]); ?></a></li><?php endforeach; endif; ?>
+      </ul>
+    </div>
+  </div>
+</header>
+<section>
+
+</section>
+</body>
+<script src="/Public/js/jquery.js"></script>
+<script src="/Public/js/count.js"></script>
+</html>
